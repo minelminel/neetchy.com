f5bfbcb88de3364d41e32ac261a69125a0bab4aa
Michael Lawrenson <ctrlcmdspace@gmail.com>
2023-08-08 01:07:23 -0400

    Hmm

--- webapp/src/components/Marquee.jsx
+++ webapp/src/components/Marquee.jsx
@@ -3,13 +3,13 @@ import Container from 'react-bootstrap/Container';
 import artwork from '../static/img/jerry2.jpeg';
 
 export default function Marquee() {
-  // if our visible screen height (minus some offset) is greater
+  // TODO: if our visible screen height (minus some offset) is greater
   // than the heigh of the image, resize the image so the whole
   // thing is visible without scrolling on mobile
-  const [width, height] = [500, 500];
 
-  const innerWidth = window.innerWidth;
-  const innerHeight = window.innerHeight;
+  // const [width, height] = [500, 500];
+  // const innerWidth = window.innerWidth;
+  // const innerHeight = window.innerHeight;
 
   const style = { textAlign: `center` };
 
